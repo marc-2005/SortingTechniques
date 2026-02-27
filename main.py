c872ca39348961dbb7def8ae739c686df68761c1
ChrisNader <chrisnader25@gmail.com>
2026-02-27 21:30:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,15 @@ def selection_sort(arr, size):
                 min_index = j
         arr[i], arr[min_index] = arr[min_index], arr[i]
 
+def insertionSort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
 if __name__ == "__main__":
     arr = [64, 25, 12, 22, 11]
     size = len(arr)
